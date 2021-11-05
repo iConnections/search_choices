032a1214718a154a11402f4a2280b1dd35ec0321
kevmiclee12 <87021378+kevmiclee12@users.noreply.github.com>
2021-11-05 07:09:13 -0400

    Update search_choices.dart

--- lib/search_choices.dart
+++ lib/search_choices.dart
@@ -1914,7 +1914,7 @@ class _DropdownDialogState<T> extends State<DropdownDialog> {
                             child: Icon(
                               Icons.clear,
                               size: 17,
-                                widget.isDark ? Color(0xFF793392) : Color(0xFF692B7E),
+                                color: widget.isDark ? Color(0xFF793392) : Color(0xFF692B7E),
                             )),
                       ),
                     )));
